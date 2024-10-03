a3e97f45ba31de69a40b3c3410e7bb80fd23f3fa
ella-an2 <ella.an@mths.ca>
2024-10-03 23:28:01 +0000

    Wrote the program

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -5,6 +5,70 @@ This module is a Micro:bit MicroPython program
 """
 
 from microbit import *
+from time import *
 
+display.clear()
+display.show(Image.HAPPY)
 
-display.scroll("Hello, World!")
+while True:
+    if button_a.is_pressed():
+        pin13.write_digital(1)
+        pin14.write_digital(0)
+        display("Blue")
+        pin13.write_digital(0)
+
+        time(1)
+
+        pin14.write_digital(1)
+        pin13.write_digital(0)
+        pin15.write_digital(0)
+        display("Green")
+        pin14.write_digital(0)
+        
+        time(1)
+
+        pin15.write_digital(1)
+        pin14.write_digital(0)
+        pin13.write_digital(0)
+        display("Red")
+        pin15.write_digital(0)
+
+        time(1)
+
+        pin14.write_digital(1)
+        pin15.write_digital(1)
+        pin13.write_digital(0)
+        display("Yellow")
+        pin14.write_digital(0)
+        pin15.write_digital(0)
+
+        time(1)
+
+        pin15.write_digital(1)
+        pin13.write_digital(1)
+        pin14.write_digital(0)
+        display("Purple")
+        pin15.write_digital(0)
+        pin13.write_digital(0)
+
+        time(1)
+
+        pin14.write_digital(1)
+        pin13.write_digital(1)
+        pin15.write_digital(0)
+        display("Cyan")
+        pin14.write_digital(0)
+        pin13.write_digital(0)
+
+        time(1)
+
+        pin14.write_digital(1)
+        pin13.write_digital(1)
+        pin15.write_digital(0)
+        display("White")
+        pin14.write_digital(0)
+        pin13.write_digital(0)
+        pin15.write_digital(0)
+
+        time(1)
+        display.show(Image.HAPPY)
