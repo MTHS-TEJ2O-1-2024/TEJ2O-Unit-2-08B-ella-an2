301c74e0a3142988084e9b5941a101bb76955288
ella-an2 <ella.an@mths.ca>
2024-10-03 19:08:08 -0400

    Finished programming

--- main.ts
+++ main.ts
@@ -10,18 +10,24 @@ basic.showIcon(IconNames.Happy)
 
 input.onButtonPressed(Button.A, function () {
     pins.digitalWritePin(DigitalPin.P13, 1)
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P15, 0)
     basic.showString("Blue")
     pins.digitalWritePin(DigitalPin.P13, 0)
 
     pause(1)
 
     pins.digitalWritePin(DigitalPin.P14, 1)
+    pins.digitalWritePin(DigitalPin.P13, 0)
+    pins.digitalWritePin(DigitalPin.P15, 0)
     basic.showString("Green")
     pins.digitalWritePin(DigitalPin.P14, 0)
 
     pause(1)
 
     pins.digitalWritePin(DigitalPin.P15, 1)
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P13, 0)
     basic.showString("Red")
     pins.digitalWritePin(DigitalPin.P15, 0)
 
@@ -29,6 +35,7 @@ input.onButtonPressed(Button.A, function () {
 
     pins.digitalWritePin(DigitalPin.P15, 1)
     pins.digitalWritePin(DigitalPin.P14, 1)
+    pins.digitalWritePin(DigitalPin.P13, 0)
     basic.showString("Yellow")
     pins.digitalWritePin(DigitalPin.P15, 0)
     pins.digitalWritePin(DigitalPin.P14, 0)
@@ -37,13 +44,30 @@ input.onButtonPressed(Button.A, function () {
 
     pins.digitalWritePin(DigitalPin.P15, 1)
     pins.digitalWritePin(DigitalPin.P13, 1)
-    basic.showString("Magenta")
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    basic.showString("Purple")
     pins.digitalWritePin(DigitalPin.P15, 0)
     pins.digitalWritePin(DigitalPin.P13, 0)
 
     pause(1)
 
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    pins.digitalWritePin(DigitalPin.P13, 1)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    basic.showString("Cyan")
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P13, 0)
 
+    pause(1)
 
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    pins.digitalWritePin(DigitalPin.P13, 1)
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    basic.showString("White")
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P13, 0)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+
+    pause(1)
+    basic.showIcon(IconNames.Happy)
 })
-
